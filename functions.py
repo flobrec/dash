c671a203ef834fef73abc330bf35519d82c6a632
all <all@perezoso.ch>
2020-04-03 15:41:15 +0200

    test old version

--- functions.py
+++ functions.py
@@ -11,23 +11,21 @@ import plotly.express as px
 from urllib.request import urlopen
 
 
-def plot_choropleth(map_data, val_json, val_loc, val_color, val_frame, val_title=''):
+def plot_choropleth(map_data, val_json, val_loc, val_color, val_frame):
     max_color = max(map_data[val_color]) 
     fig = px.choropleth_mapbox(map_data, geojson=val_json, locations=val_loc, color=val_color,
                             color_continuous_scale="peach",
                             range_color=(0, max_color),
                             mapbox_style="carto-darkmatter",
                             zoom=6, center = {"lat": 47.05048, "lon": 8.30635},
-                            opacity=1,
+                            opacity=0.5,
                             height=500,
                             #labels={'Cases':'Confirmed cases'},
                             animation_frame=val_frame,
                             template="plotly_dark",
-                            title={'text':val_title},
-                            #hover_name='Date',
+                            #hover_name='Date'
                           )
-    fig.update_layout(margin={"r":0,"t":50,"l":0,"b":0})
-    #fig.update_layout(title='Default title')
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     return fig
 
 def plot_bar(bar_data, val_x, val_y, val_color, val_text, val_title='',val_scale='linear'):
@@ -61,9 +59,3 @@ def plot_line(data, val_x, val_y, val_color, val_text='', val_title=''):
              )
     fig.update_xaxes(tickangle=-90, showticklabels=True, type = 'category')
     return fig
-
-def format_diff_line(val_diff):
-        return '{:+.0f}'.format(val_diff)
-    
-def format_pct_line(val_pct):
-    return '{0:+.1%}'.format(val_pct)
